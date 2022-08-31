d5466ee284b8be38aa4e475a6bb512fb4bf48e9f
Narcisse <106417952+Narcisse@users.noreply.github.com>
2022-08-31 14:06:24 -0500

    1st Commit

---
+++ src/test/java/tests/HomePageTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import pages.HomePagePage;
+import util.BrowserFactory;
+
+public class HomePageTest {
+
+	WebDriver driver;
+	
+	@Test
+	public void userShouldBeAbleToAddItem() {
+		
+		 driver = BrowserFactory.init();
+		 HomePagePage homepage = PageFactory.initElements(driver, HomePagePage.class);
+		 homepage.addItems();
+		 homepage.checkToggleAllBox();
+		 homepage.selectAndRemoveOneItem();
+		 homepage.selectAndRemoveAllItems();
+		
+		
+	}
+	
+}
